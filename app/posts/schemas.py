7329a8a29ac52ececa2501d558aab07297c21a9d
artemioo <artemryb1@gmail.com>
2023-09-24 14:07:17 +0300

    create PostEditSchema

--- app/posts/schemas.py
+++ app/posts/schemas.py
@@ -2,9 +2,13 @@ from pydantic import BaseModel
 
 
 class PostCreateSchema(BaseModel):
-    id: int
     title: str
     body: str
 
     # class Config:
     #     orm_mode = True
+
+
+class PostEditSchema(BaseModel):
+    title: str
+    body: str
