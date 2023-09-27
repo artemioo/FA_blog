edd0e07e83b36690740e704a91f11667233365a7
artemioo <artemryb1@gmail.com>
2023-09-27 10:56:47 +0300

    create exceptions for User

---
+++ app/users/exceptions.py
@@ -0,0 +1,23 @@
+from fastapi import HTTPException
+
+
+class LoginRequiredException(HTTPException):
+    pass
+
+
+class UserHasAccountException(Exception):
+    """
+    User already has account
+    """
+
+
+class InvalidEmailException(Exception):
+    """
+    Invalid email
+    """
+
+
+class InvalidUserIDException(Exception):
+    """
+    Invalid user id
+    """
